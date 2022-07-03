f8cfa85de43e3a223b17f11ab30a1815a3267e10
Progyan <bprogyan@gmail.com>
2022-07-03 20:29:38 +0530

    (chore) Workspace: Initial Commit
    Signed-off-by: Progyan <bprogyan@gmail.com>

---
+++ src/main/java/thepro/dev/springcleanexample/SpringCleanExampleApplication.java
@@ -0,0 +1,13 @@
+package thepro.dev.springcleanexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCleanExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCleanExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thepro/dev/springcleanexample/SpringCleanExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package thepro.dev.springcleanexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCleanExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
